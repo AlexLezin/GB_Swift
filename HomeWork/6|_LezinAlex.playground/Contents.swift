ed023eb63a64c081cb6c0d759d8de36fcc2ec85c
lezin <reg@lezin.me>
2019-01-26 18:25:39 +0300

    Sixth homework

--- HomeWork/6|_LezinAlex.playground/Contents.swift
+++ HomeWork/6|_LezinAlex.playground/Contents.swift
@@ -1,14 +1,44 @@
-//: A Cocoa based Playground to present user interface
+//1. Реализовать свой тип коллекции «очередь» (queue) c использованием дженериков.
+//2. Добавить ему несколько методов высшего порядка, полезных для этой коллекции (пример: filter для массивов)
+//3. * Добавить свой subscript, который будет возвращать nil в случае обращения к несуществующему индексу.
 
-import AppKit
-import PlaygroundSupport
+struct Queue<T: Equatable> {
+    private var elements: [T] = []
+    mutating func push(_ elts: T...) {
+        for i in elts {
+            elements.append(i)
+        }
+    }
+    mutating func pop() -> T? {
+        return elements.removeFirst()
+    }
+    mutating func filter(_ by: T) {
+        for element in elements {
+            if element != by {
+                elements.remove(at: elements.index(of: element)!)
+            }
+        }
+    }
+    mutating func size() -> Int {
+        var count = 0
+        for _ in elements {
+            count += 1
+        }
+        return count
+    }
+    subscript(ind: Int) -> T? {
+        if ind > elements.index(of: elements.last!)! {
+            return nil
+        } else {
+            return elements[ind]
+        }
+    }
+}
 
-let nibFile = NSNib.Name("MyView")
-var topLevelObjects : NSArray?
+var newQ = Queue<Int>()
 
-Bundle.main.loadNibNamed(nibFile, owner:nil, topLevelObjects: &topLevelObjects)
-let views = (topLevelObjects as! Array<Any>).filter { $0 is NSView }
+newQ.push(4, 6, 4, 6, 7, 3, 6, 4, 7, 3)
 
-// Present the view in Playground
-PlaygroundPage.current.liveView = views[0] as! NSView
+newQ.size()
 
+newQ[10]
